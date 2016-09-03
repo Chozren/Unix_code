475ce51aefef6fa8f186b66f464c0d7a4b5a3b6d
Chozren <15277186611@163.com>
2016-09-03 19:47:42 +0800

    new

--- U-L/more/more1.c
+++ U-L/more/more1.c
@@ -2,6 +2,7 @@
  * read and print 24 lines then pause for a few special commands
  */ 
 #include<stdio.h>
+#include<stdlib.h>
 #define PAGELEN 24
 #define LINELEN 512
 
@@ -14,7 +15,7 @@ int main(int argc, char *argv[])
     if (argc == 1)
         do_more(stdin);
     else
-        while (--agrc)
+        while (--argc)
             if ((fp = fopen(* ++argv, "r")) != NULL)
             {
                 do_more(fp);
@@ -37,13 +38,33 @@ void do_more(FILE *fp)
 
     while (fgets(line, LINELEN, fp)) {
         if (num_of_lines == PAGELEN) {  
-            reply == see_more();
+            reply = see_more();
             if (reply == 0)
                 break;
             num_of_lines -= reply;    
         }
-
+        if (fputs(line, stdout) == EOF)
+            exit(1);
+        num_of_lines++;
     }
-
 } 
+int see_more()
+/*
+* print message, wait for response, return # of lines to advance
+* q means no, space means yes, CR means one line 
+*/ 
+{
+    int c;
+
+    printf("\033[7m more?\033[m");
+    while ((c = getchar()) != EOF) {
+        if (c == 'q')
+            return 0;
+        if (c == ' ')
+            return PAGELEN;
+        if (c == '\n')
+            return 1;
+    }
+    return 0;
+}
 
